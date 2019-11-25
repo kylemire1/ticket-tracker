2b8cd5a53dc486923f04bf40d3d084b9f7702678
Kyle Lemire <kyle@elykinnovation.com>
2019-11-25 15:47:05 -0600

    Fixed mobile sizing of the login form card

--- src/components/login.js
+++ src/components/login.js
@@ -50,41 +50,41 @@ const Login = props => {
 
   return (
     <>
-      {user.name && user.email ? <Redirect to="/" /> : null}
+      {user.name && user.email ? <Redirect to='/' /> : null}
       <Container style={{ height: "100vh" }}>
-        <Row className="h-100 justify-content-center align-items-center">
-          <Card className="p-3 col-6">
-            <h1 className="text-center mb-3">Login</h1>
+        <Row className='h-100 justify-content-center align-items-center'>
+          <Card className='p-3' lg='6' md='12'>
+            <h1 className='text-center mb-3'>Login</h1>
             <ShardsForm onSubmit={e => handleSubmit(e)}>
               <FormGroup>
-                <label htmlFor="email">Email</label>
+                <label htmlFor='email'>Email</label>
                 <FormInput
-                  id="email"
-                  name="email"
+                  id='email'
+                  name='email'
                   value={formValues.email}
                   onChange={e => handleChange(e)}
                   required
                 />
               </FormGroup>
               <FormGroup>
-                <label htmlFor="password">Password</label>
+                <label htmlFor='password'>Password</label>
                 <FormInput
-                  type="password"
-                  id="password"
-                  name="password"
+                  type='password'
+                  id='password'
+                  name='password'
                   value={formValues.password}
                   onChange={e => handleChange(e)}
                   required
                 />
               </FormGroup>
-              <Button className="mb-3" style={{ width: "100%" }} type="submit">
+              <Button className='mb-3' style={{ width: "100%" }} type='submit'>
                 Log In
               </Button>
             </ShardsForm>
             {logInError !== "" ? (
-              <Alert theme="danger">{logInError}</Alert>
+              <Alert theme='danger'>{logInError}</Alert>
             ) : null}
-            <Alert theme="light">
+            <Alert theme='light'>
               <p style={{ fontWeight: "bold" }}>
                 Guest login credentials with read-only permissions:
               </p>
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-import axios from "axios";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCcKlsmoCMGjUnNlkJt4yE6nfYRqVN0MjA",
